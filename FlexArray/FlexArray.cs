8516d0f77b8bda8b4c1c4a1eeba6be792869a4d4
MarleyDey <marleydey11@gmail.com>
2020-06-12 17:42:12 +0100

    Added comments to the Flex Array class

--- FlexArray/FlexArray.cs
+++ FlexArray/FlexArray.cs
@@ -4,6 +4,15 @@ namespace FlexArray
 {
     public static class FlexArray
     {
+        /**
+         * <summary>
+         *  This method is used to fill an array with
+         *  a generic type.
+         *
+         *  <param name="array">The array to be filled</param>
+         *  <param name="value">The value to fill array with</param>
+         * </summary>
+         */
         public static void Fill<TArray>(this TArray[] array, TArray value)
         {
             for (var i = 0; i < array.Length; i++)
@@ -12,6 +21,17 @@ namespace FlexArray
             }
         }
 
+        /**
+         * <summary>
+         *  This method is used to replace an array with
+         *  a generic type value of the same type as the
+         *  array.
+         *
+         *  <param name="array">The array with value to replace</param>
+         *  <param name="value">The value to replace</param>
+         *  <param name="replaceValue">The value to be replaced with</param>
+         * </summary>
+         */
         public static void Replace<TArray>(this TArray[] array, TArray value, TArray replaceValue)
         {
             for (var i = 0; i < array.Length; i++)
@@ -24,6 +44,16 @@ namespace FlexArray
             }
         }
 
+        /**
+         * <summary>
+         *  This method is used to replace all values of a specific
+         *  value with another value of the array type.
+         *
+         *  <param name="array">The array with value to replace</param>
+         *  <param name="value">The value(s) to replace</param>
+         *  <param name="replaceValue">The value(s) to be replaced with</param>
+         * </summary>
+         */
         public static void ReplaceAll<TArray>(this TArray[] array, TArray value, TArray replaceValue)
         {
             for (var i = 0; i < array.Length; i++)
